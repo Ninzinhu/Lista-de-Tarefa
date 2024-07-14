2a343c11d480630c2c90707475074e500e283936
João <pel118201@gmail.com>
2024-07-14 20:54:46 -0300

    chore: add new task in HTML

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,17 +2,24 @@ const form = document.querySelector("#Todo-form");
 const taskTitleinput = document.querySelector("#task-title-input");
 const todoListUl = document.querySelector("#todo-list-ul");
 
+let tasks = [];
 
-form.addEventListener('submit', (submit)=> {
-  submit.preventDefault() // Evita o comportamento padrão de recarregar a pagina ao submeter o form
+form.addEventListener("submit", (submit) => {
+  submit.preventDefault(); // Evita o comportamento padrão de recarregar a pagina ao submeter o form
 
- const taskTitle = taskTitleinput.value
+  const taskTitle = taskTitleinput.value;
 
- if (taskTitle.length < 3){
-  alert('Sua tarefa precisa ter pelo menos 3 caracteres.')
-  return;
+  if (taskTitle.length < 3) {
+    alert("Sua tarefa precisa ter pelo menos 3 caracteres.");
+    return;
+  }
 
+  // Adicionando a nova tarefa no Array de tasks
+  tasks.push(taskTitle);
 
- }
+  // Adicionando a nova tarefa no HTML
+  const li = document.createElement("li");
+  li.textContent = taskTitle; // <li>Tarefa2</li>
 
-})
+  todoListUl.appendChild(li);
+});
