d4021eec53c1a2113ec15dcb76708074e0557a38
João <pel118201@gmail.com>
2024-07-14 18:54:21 -0300

    chore: block reload page

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,4 +3,8 @@ const taskTitleinput = document.querySelector("#task-title-input");
 const todoListUl = document.querySelector("#todo-list-ul");
 
 
+form.addEventListener('submit', (submit)=> {
+  submit.preventDefault() // Evita o comportamento padrão de recarregar a pagina ao submeter o form
 
+
+})
