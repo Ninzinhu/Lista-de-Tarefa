1099f1efd76dfb49200ea5cf1a74983f92a2eeab
João <pel118201@gmail.com>
2024-07-14 18:49:33 -0300

    chore : get tags html

---
+++ public/scripts/index.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("#Todo-form");
+const taskTitleinput = document.querySelector("#task-title-input");
+const todoListUl = document.querySelector("#todo-list-ul");
+
+
+
